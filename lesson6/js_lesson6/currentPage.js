9cecb02f1827950d380f07fc02a09f5383e5828d
BrowCow <cow18003@byui.edu>
2020-02-13 10:17:27 -0800

    lesson6 push1

---
+++ lesson6/js_lesson6/currentPage.js
@@ -0,0 +1,14 @@
+let currentPage = window.location.href;
+
+if (currentPage = preston_6.html) {
+  document.getElementById('preston').style.color = 'darkOrange';
+};
+
+
+/*
+document.getElementById('preston').style.color = 'darkOrange';
+document.getElementById('soda').style.color = 'darkOrange';
+document.getElementById('fish').style.color = 'darkOrange';
+document.getElementById('storm').style.color = 'darkOrange';
+document.getElementById('gallery').style.color = 'darkOrange';
+*/
---
+++ lesson6/js_lesson6/pancakesBanner.js
@@ -0,0 +1,5 @@
+let d = new Date();
+let day = d.getDay();
+if (day == 4) {
+  document.getElementById('banner').style.display='none';
+}
