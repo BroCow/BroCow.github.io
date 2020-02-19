fe5f790c428873e85715fdee15d6ac7a6215667b
BrowCow <cow18003@byui.edu>
2020-02-19 14:47:52 -0800

    lazyload push

--- lesson6/js_lesson6/pancakesBanner.js
+++ lesson6/js_lesson6/pancakesBanner.js
@@ -1,5 +1,10 @@
 let d = new Date();
 let day = d.getDay();
-if (day == 4) {
-  document.getElementById('banner').style.display='none';
+if (day == 5) {
+  document.getElementById('banner').style.display='block';
 }
+
+
+/*
+document.getElementById('banner').style.display='none';
+*/
--- test/daisy_js/daisy_button.js
+++
@@ -1,9 +0,0 @@
-
-
-function showDaisy() {
-    document.getElementById('test').style.display='block';
-}
-
-function hideDaisy() {
-    document.getElementById('test').style.display='none';
-}
