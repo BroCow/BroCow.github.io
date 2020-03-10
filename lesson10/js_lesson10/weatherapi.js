90c922f4240deb7c144f63476086900e320278ec
BrowCow <cow18003@byui.edu>
2020-03-10 13:15:49 -0700

    API Activity

---
+++ lesson10/js_lesson10/weatherapi.js
@@ -0,0 +1,10 @@
+
+
+
+const apiURL = api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fbc04d5ec42da978089a013c07d62838;
+
+fetch(apiURL)
+    .then((Response) => Response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+    });
