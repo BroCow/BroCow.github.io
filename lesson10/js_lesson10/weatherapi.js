4ea8f8f5882ca85965238dd9d3fc9e7befc6c1e9
BrowCow <cow18003@byui.edu>
2020-03-11 16:52:06 -0700

    API Test push

--- lesson10/js_lesson10/weatherapi.js
+++ lesson10/js_lesson10/weatherapi.js
@@ -1,7 +1,7 @@
 
 
 
-const apiURL = api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fbc04d5ec42da978089a013c07d62838;
+const apiURL = "api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fbc04d5ec42da978089a013c07d62838";
 
 fetch(apiURL)
     .then((Response) => Response.json())
