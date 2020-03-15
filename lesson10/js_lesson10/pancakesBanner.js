31b19c4f2d0b3b932a3002b8acb38ce8f8683007
BrowCow <cow18003@byui.edu>
2020-03-14 19:17:20 -0700

    lesson10 preston

---
+++ lesson10/js_lesson10/pancakesBanner.js
@@ -0,0 +1,10 @@
+let d = new Date();
+let day = d.getDay();
+if (day == 5) {
+  document.getElementById('banner').style.display='block';
+}
+
+
+/*
+document.getElementById('banner').style.display='none';
+*/
--- lesson10/js_lesson10/weatherapi.js
+++ lesson10/js_lesson10/weatherapi.js
@@ -1,10 +1,18 @@
 
 
-
-const apiURL = "api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fbc04d5ec42da978089a013c07d62838";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=fbc04d5ec42da978089a013c07d62838";
 
 fetch(apiURL)
     .then((Response) => Response.json())
     .then((jsObject) => {
         console.log(jsObject);
+    
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
     });
+    
