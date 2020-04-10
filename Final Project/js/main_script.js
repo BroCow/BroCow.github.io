238850b0a99e61df4666694104936619ad35b026
BrowCow <cow18003@byui.edu>
2020-04-10 11:56:16 -0700

    final project push 4.10

--- Final Project/js/main_script.js
+++ Final Project/js/main_script.js
@@ -6,29 +6,81 @@ function toggleMenu() {
     document.querySelector(".navigation").classList.toggle("responsive");
 }
 
-//Riverguide Jason Info
+//Riverguide JSON Info
+
 /*Store resource - JSON file from lesson - into variable*/
+const requestURL = 'https://raw.githubusercontent.com/BroCow/BroCow.github.io/master/Final%20Project/js/river_guides.json';
 
 
-const riverguideJson = js/river_guides.json;
 
-
-fetch(riverguideJson)
+fetch(requestURL)
+  //Returns promise which response we will work with as an argument to an asynchronous function
   .then(function (response) {
+    //We need to extract the JSON content from the noise of the full HTTP response using the json() method
     return response.json();
   })
+  //Sets us up to work with the converted response data in Javascript object format
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
 
-
+    //Store the results of the converted response into an array - MUST MATCH NAME OF JSON
     const guides = jsonObject['guides'];
 
-    for (let i = 0; i < guides.length; i++ ) {
-      let card = document.createElement('section');
+    //Need to loop through every record and process them into their own 'cards' using FOR LOOP
+    //IMPORTANT: Put this line of code directly after the console.table() line and before you close the .then() method });
+    for (let i = 0; i < guides.length; i++) {
 
+      //Now build the HTML of card using the createElement(), textContent(), and appendChild() methods
+      //Make each 'card' a <section> element with standard <h2> and <p> for field items and an <img>
+      let card = document.createElement('section');
+      
       let h2 = document.createElement('h2');
       h2.textContent = guides[i].firstname + ' ' + guides[i].lastname;
       card.appendChild(h2);
+
+      let cert = document.createElement('p');
+      cert.textContent = 'Certification Level:' + ' ' + guides[i].certlevel;
+      card.appendChild(cert);
+
+      let yearsOfExperience = document.createElement('p');
+      yearsOfExperience.textContent = 'Years of Experience:' + ' ' + guides[i].experience;
+      card.appendChild(yearsOfExperience);
+
+      let email = document.createElement('p');
+      email.textContent = 'Email:' + ' ' + guides[i].email;
+      card.appendChild(email);
+
+      let bio = document.createElement('p');
+      bio.textContent = 'Biography:' + ' ' + guides[i].bio;
+      card.appendChild(bio);
+
+      let image = document.createElement('img');
+      image.setAttribute('src', guides[i].image);
+      card.appendChild(image);
+
+      document.querySelector('div.cards').appendChild(card);
+    }
+  });
+    
+  
+
+  
+  
+    
+    
+
+
+
+
+
+    
+
+    //for (let i = 0; i < guides.length; i++ ) {
+      //let card = document.createElement('section');
+
+      //let h2 = document.createElement('h2');
+      //h2.textContent = guides[0].firstname + ' ' + guides[i].lastname;
+      //card.appendChild(h2);
 
       
       /*Create variables to store info into "p" elements*/
@@ -53,5 +105,4 @@ fetch(riverguideJson)
 
       //document.querySelector('div.cards').appendChild(card);
       
-    }
-  });
+    //}
